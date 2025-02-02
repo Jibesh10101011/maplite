57d349e5e41ab4009bb289f35d659282e8c9016d
Jibesh-dev-89 <jibeshr60@gmail.com>
2025-02-02 13:19:19 +0530

    Room created done with searching functionality

--- backend/routes/room.js
+++ backend/routes/room.js
@@ -1,9 +1,10 @@
 const { Router } = require("express");
-const { handleGenerateRoomId, handleCreateRoom, handleValidateRoom } = require("../controllers/room");
+const { handleGenerateRoomId, handleCreateRoom, handleValidateRoom, getAllRooms } = require("../controllers/room");
 const router = Router();
 
 router.route("/genId").get(handleGenerateRoomId);
 router.route("/create").post(handleCreateRoom);
 router.route("/validate").post(handleValidateRoom);
+router.route("/all").get(getAllRooms);
 
 module.exports=router;
