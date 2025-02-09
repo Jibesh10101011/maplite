89e1167309a62744231096aa3fe6d4eb64502f16
Jibesh-dev-89 <jibeshr60@gmail.com>
2025-02-09 18:43:56 +0530

    Socket.io shifted to another file

--- backend/index.js
+++ backend/index.js
@@ -6,66 +6,18 @@ const bodyParser = require("body-parser");
 const kafkaRoute = require("./routes/kafka");
 const authRoute = require("./routes/auth");
 const roomRoute = require("./routes/room");
+const redisRoute = require("./routes/test");
 const { connectConsumer } = require("./consumer");
 const mongoose = require("mongoose");
-const socketIo = require("socket.io");
 const http = require("http");
-const Redis = require("ioredis");
 const MONGO_URL = process.env.MONGO_URL;
-const REDIS_URL = process.env.REDIS_URL;
-
-
-const redis = new Redis(REDIS_URL);
-
+const socketIo = require("./socket");
 const app = exprees();
 const PORT = process.env.PORT;
 
 const server = http.createServer(app);
-const io = socketIo(server,{cors:"*"});
-
-
-
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  
-
-  socket.on("subscribe",async(roomId)=>{
-    if(roomId) {
-      socket.join(roomId);
-      console.log(`Client joined room : ${roomId}`);
-
-      // Fetch last 50 messages from Redis (list)
-      const messages = await redis.lrange(`chat:${roomId}`,0,-1);
-      const parsedMessages = messages.map(msg=>JSON.parse(msg));
-      socket.emit('previous_messages',parsedMessages);
-
-    }
-  });
-
-  // Handle message sending
-
-  socket.on('send_message', async({roomId,message,sender}) => {
-    if(roomId && message) {
-
-      const newMessage = { sender,message,timestamp: new Date().toISOString() };
-
-      // Save message to Redis list (max 50 messages per room)
-
-      await redis.rpush(`chat:${roomId}`,JSON.stringify(newMessage));
-      await redis.ltrim(`chat:${roomId}`,-50,-1);
-
-      console.log(`Message saved to ${roomId}: `,newMessage);
-
-      io.to(roomId).emit('chat_message', newMessage);
-    } else {
-      console.log("Room ID or message is missing.");
-    }
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
+// Initialize socket.io
+socketIo(server);
 
 
 async function connectDb() {
@@ -94,6 +46,7 @@ app.use(bodyParser.json());
 app.use("/auth",authRoute);
 app.use("/api",kafkaRoute);
 app.use("/room",roomRoute);
+app.use("/redis",redisRoute);
 
 
 server.listen(PORT, () => {
