50a1f698a5aebec8ef65aa5012b3d3d85879755e
Jibesh@dev <114723590+Jibesh10101011@users.noreply.github.com>
2025-11-06 01:15:19 +0530

    Increase JSON body limit to 16kb

--- backend/api-server/src/app.ts
+++ backend/api-server/src/app.ts
@@ -15,7 +15,7 @@ app.use(
     })
 );
 
-app.use(express.json());
+app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: process.env.URL_ENCODED_LIMIT! }));
 app.use(express.static("public"));
 app.use(cookieParser());
